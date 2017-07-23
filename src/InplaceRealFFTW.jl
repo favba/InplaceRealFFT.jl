d23bcd0883c4ddb9c1e0b80078d5eac8a802a500
Felipe <felipe.avba@gmail.com>
2017-07-23 01:56:44 -0400

    Intial Version
    Basic functionality for module and basic tests comparing to out-of-place ffts.

--- src/InplaceRealFFTW.jl
+++ src/InplaceRealFFTW.jl
@@ -1,5 +1,160 @@
+__precompile__()
 module InplaceRealFFTW
 
-# package code goes here
+import Base: size, IndexStyle, getindex, setindex!, eltype, *, /, \, similar, copy, broadcast, real, complex
+
+export PaddedArray , plan_rfft!, rfft!, plan_irfft!, irfft!
+
+const Float3264 = Union{Float32,Float64}
+
+struct PaddedArray{T<:Float3264,N} <: AbstractArray{Complex{T},N}
+  c::Array{Complex{T},N} # Complex view of the array
+  r::SubArray # Real view skipping padding
+  rr::Array{T,N} # Raw real data, including padding
+
+  function PaddedArray{T,N}(rr::Array{T,N},nx::Int) where {T<:Float3264,N}
+    fsize = [size(rr)...]
+    iseven(fsize[1])|| throw(ArgumentError("First dimension of allocated array must have even number of elements")) # change to `assumption || throw(ArgumentError("…")`
+    (nx == fsize[1]-2 || nx == fsize[1]-1) || throw(ArgumentError("Number of elements on the first dimension of array must be either 1 or 2 less than the number of elements on the first dimension of the allocated array"))
+    fsize[1] = fsize[1]/2
+    rsize = (fsize...)
+    c = reinterpret(Complex{T}, rr, rsize)
+    fsize[1] = nx
+    r = view(rr,(1:l for l in fsize)...)
+    return new{T,N}(c,r,rr)
+  end # function
+
+  function PaddedArray{T,N}(c::Array{Complex{T},N},nx::Int) where {T<:Float3264,N}
+    fsize = [size(c)...]
+    (iseven(nx) ? fsize[1]*2-2 == nx : fsize[1]*2-1 == nx) || throw(ArgumentError("The allocated array does not have the proper padding"))
+    fsize[1] = fsize[1]*2
+    rr = reinterpret(T, c, (fsize...))
+    fsize[1] = nx
+    r = view(rr,(1:l for l in fsize)...)
+    return new{T,N}(c,r,rr)
+  end # function
+
+end # struct
+
+PaddedArray(rr::Array{T,N},nx::Int) where {T,N} = PaddedArray{T,N}(rr::Array{T,N},nx::Int)
+PaddedArray(r::SubArray{T,N,P,I,L}) where {T,N,P,I,L} = PaddedArray{T,N}(r::SubArray{T,N,P,I,L})
+PaddedArray(c::Array{Complex{T},N},nx::Int) where {T,N} = PaddedArray{T,N}(c::Array{Complex{T},N},nx::Int)
+
+size(S::PaddedArray) = size(S.c)
+IndexStyle(::Type{T}) where {T<:PaddedArray} = IndexLinear()
+Base.@propagate_inbounds getindex(S::PaddedArray,I...) = S.c[I...]
+Base.@propagate_inbounds setindex!(S::PaddedArray,v,I) =  setindex!(S.c,v,I)
+eltype(S::PaddedArray{T,N}) where {T,N} = Complex{T} 
+copy(S::PaddedArray) = PaddedArray(copy(S.c),size(S.r)[1])
+similar(f::PaddedArray) = PaddedArray(eltype(f.r),size(f.r))
+real(S::PaddedArray) = S.r
+complex(S::PaddedArray) = S.c
+broadcast(op::Function, A::PaddedArray, other) = broadcast(op, A.c, other)
+broadcast(op::Function, other, A::PaddedArray) = broadcast(op, other, A.c)
+broadcast(op::Function, A::PaddedArray, other::PaddedArray) = broadcast(op, other.c, A.c)
+broadcast(op::Function, A::PaddedArray) = broadcast(op, A.c)
+
+function PaddedArray(t::DataType,ndims::Vararg{Integer,N}) where N
+  fsize = [ndims...]
+  iseven(fsize[1]) ? fsize[1]+=2 : fsize[1]+=1
+  a = Array{t,N}((fsize...))
+  PaddedArray(a,ndims[1])
+end
+
+PaddedArray(t::DataType,ndims::NTuple{N,Integer}) where N = PaddedArray(t,ndims...)
+PaddedArray(ndims::Vararg{Integer,N}) where N = PaddedArray(Float64,ndims)
+PaddedArray(ndims::NTuple{N,Integer}) where N = PaddedArray(Float64,ndims...)
+
+function PaddedArray(a::Array{T,N}) where {T<:Float3264,N}
+  t = PaddedArray(T,size(a))
+  t.r .= a 
+  return t
+end
+
+
+randt(dims...) = PaddedArray(iseven(dims[1]) ? rand(dims[1]+2,dims[2:end]...) : rand(dims[1]+1,dims[2:end]...), dims[1])
+
+function readfield!(filename::String,f::PaddedArray{T,3}) where {T<:Union{Float32,Float64}}
+  nx,ny,nz = size(f.r)
+  dtype,padded = checkinput(filename,nx,ny,nz)
+  dtype == T || error("Different Float precisions")
+  if padded
+    read!(filename,f.rr)
+  else
+    rr = f.rr
+    sb = sizeof(T)
+    dimm = (nx+2,ny,nz)
+    open(filename) do file
+      for k in 1:nz
+        for j in 1:ny
+          unsafe_read(file,pointer(rr,sub2ind(dimm,1,j,k)),nx*sb)
+        end
+      end
+    end
+  end
+  return f
+end
+###########################################################################################
+
+function plan_rfft!(X::PaddedArray{T,N}, region;
+                   flags::Integer=FFTW.ESTIMATE,
+                   timelimit::Real=FFTW.NO_TIMELIMIT) where {T<:Float3264,N}
+
+  (1 in region) || throw(ArgumentError("The first dimension must always be transformed"))
+  if flags&FFTW.ESTIMATE != 0
+    p = FFTW.rFFTWPlan{T,FFTW.FORWARD,true,N}(X.r, X.c, region, flags, timelimit)
+  else
+    x = similar(X)
+    p = FFTW.rFFTWPlan{T,FFTW.FORWARD,true,N}(x.r, x.c, region, flags, timelimit)
+  end
+  return p
+end
+
+plan_rfft!(f::PaddedArray;kws...) = plan_rfft!(f,1:ndims(f.r);kws...)
+
+*(p::FFTW.rFFTWPlan{T,FFTW.FORWARD,true,N},f::PaddedArray{T,N}) where {T<:Float3264,N} = (A_mul_B!(f.c,p,f.r);f)
+
+rfft!(f::PaddedArray, region=1:ndims(f.r)) = plan_rfft!(f,region) * f
+
+
+##########################################################################################
+
+function plan_brfft!(X::PaddedArray{T,N}, region;
+                    flags::Integer=FFTW.PRESERVE_INPUT,
+                    timelimit::Real=FFTW.NO_TIMELIMIT) where {T<:Float3264,N}
+  (1 in region) || throw(ArgumentError("The first dimension must always be transformed"))
+  if flags&FFTW.PRESERVE_INPUT != 0
+    a = similar(X)
+    return FFTW.rFFTWPlan{Complex{T},FFTW.BACKWARD,true,N}(a.c, a.r, region, flags,timelimit)
+  else
+    return FFTW.rFFTWPlan{Complex{T},FFTW.BACKWARD,true,N}(X.c, X.r, region, flags,timelimit)
+  end
+end
+
+function plan_irfft!(x::PaddedArray{T,N}, region; kws...) where {T,N}
+  Base.DFT.ScaledPlan(plan_brfft!(x, region; kws...),Base.DFT.normalization(T, size(x.r), region))
+end
+
+plan_irfft!(f::PaddedArray;kws...) = plan_irfft!(f,1:ndims(f.c);kws...)
+
+*(p::Base.DFT.ScaledPlan,f::PaddedArray{T,N}) where {T<:Float3264,N} = begin
+  A_mul_B!(f.r,p.p,f.c)
+  scale!(f.rr,p.scale)
+  f
+end
+
+irfft!(f::PaddedArray, region=1:ndims(f.c)) = plan_irfft!(f,region) * f
+
+##########################################################################################
+
+function /(f::PaddedArray{T,N},p::FFTW.rFFTWPlan{T,FFTW.FORWARD,true,N}) where {T<:Float3264,N}
+  isdefined(p,:pinv) || (p.pinv = plan_irfft!(f,p.region))
+  return p.pinv * f
+end
+
+function \(p::FFTW.rFFTWPlan{T,FFTW.FORWARD,true,N},f::PaddedArray{T,N}) where {T<:Float3264,N}
+  isdefined(p,:pinv) || (p.pinv = plan_irfft!(f,p.region))
+  return p.pinv * f
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,20 @@ using InplaceRealFFTW
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+a = rand(8,4,4)
+b = PaddedArray(a)
+@test rfft(a) ≈ rfft!(b) 
+@test a ≈ irfft!(b).r
+@test rfft(a,1:2) ≈ rfft!(b,1:2) 
+@test a ≈ irfft!(b,1:2).r
+@test rfft(a,(1,3)) ≈ rfft!(b,(1,3)) 
+@test a ≈ irfft!(b,(1,3)).r
+
+a = rand(9,4,4)
+b = PaddedArray(a)
+@test rfft(a) ≈ rfft!(b) 
+@test a ≈ irfft!(b).r
+@test rfft(a,1:2) ≈ rfft!(b,1:2) 
+@test a ≈ irfft!(b,1:2).r
+@test rfft(a,(1,3)) ≈ rfft!(b,(1,3)) 
+@test a ≈ irfft!(b,(1,3)).r
